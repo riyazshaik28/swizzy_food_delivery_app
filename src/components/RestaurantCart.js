4fdbe14daec56d2668f27bdc46c4018d349293b8
RIYAZ12697 <sshaikriyaz252@gmail.com>
2025-09-05 20:17:14 +0530

    Built RestaurantCart.js to handle cart operations

--- src/components/RestaurantCart.js
+++ src/components/RestaurantCart.js
@@ -1,20 +1,38 @@
-
+import { useContext } from "react";
 import { CDN_REST } from "./utils/constant";
+import { UserContext } from "./utils/usercontext";
 const Rest=(props)=>{
+
+  const { logginuser } = useContext(UserContext);
+
     const {resData}=props;
-    return(<div className="rest" >
+    return(<div className="py-[7px] px-[8px] mx-1 w-[228px] border border-dotted border-black bg-sky-50 rounded-md text-left overflow-hidden text-ellipsis whitespace-nowrap " >
       <div className="diimage">
-      <div className="divimage" >
-       <div id="m"> <img id="log"src={CDN_REST+resData.info.cloudinaryImageId} ></img></div>
-        <h3 id="h"><b>{resData.info.name}</b></h3>
-        <h3 id="n">&#11088; {resData.info.avgRating} rating</h3>
-        <h3 id="l">	&#128690;  {resData.info.sla.deliveryTime}mins</h3>
-        <h3>{resData.info.cuisines.join(',')}</h3>
+      <div className=" hover:font-bold" >
+      <div id="m" className="border border-solid border-black rounded-md hover:w-52"> <img className="border border-solid border-white rounded-md hover:w-52" id="log"src={CDN_REST+resData.info.cloudinaryImageId} ></img></div>
+        <h3 id="h"  ><b>{resData.info.name}</b></h3>
+        <h3 id="n" >&#11088; {resData.info.avgRating} rating</h3>
+        <h3 id="l" >	&#128690; {resData.info.sla.deliveryTime}mins</h3>
+        <h3 >{resData.info.cuisines.join(',')}</h3>
   
-        <h3>{resData.info.costForTwo}</h3>
-        <h1></h1>
+        <h3 >{resData.info.costForTwo}</h3>
+        <h3 >👤:{logginuser}</h3>
       </div>
       </div>
     </div>)
   }
+
+  //HIGER OREDR COMPONENT
+
+  export const Higherorderfx = (Rest) => {
+    return (props) => {
+      return (
+        <div>
+          <label className =" absolute  bg-black font-bold text-white ml-2  border border-dashed border-white rounded-lg " >promoted</label>
+          <Rest {...props} />
+        </div>
+      );
+    };
+  };
+  
   export default Rest;
