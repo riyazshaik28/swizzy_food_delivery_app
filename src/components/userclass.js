7ba631076226739989f0a50a68aeee74b33c7735
RIYAZ12697 <sshaikriyaz252@gmail.com>
2025-09-05 20:17:16 +0530

    Introduced userclass.js for class-based user component

--- src/components/userclass.js
+++ src/components/userclass.js
@@ -5,39 +5,67 @@ class UserClass extends React.Component{
         
         super(props);
         this.state={
-            count:22,
-            
-       
-        }
-        console.log(props);
-        console.log(this.props.name+"child constructor");
-    }
-
-
-componentDidMount(){
-    console.log(this.props.name+"child mount");
-}
-
-    render() {
+            info:{
+            name: "fffff",
+            location:"warangle",
+            avatar_url:"httpvjkjhvc"
+        }}
         
-        console.log(this.props.name+"child render");
-        return(
-            <div className="user-cart">
-                <h1>count={this.state.count}</h1>
-                <button className="tttt" onClick={()=>{
-                     this.setState({
-                        count:this.state.count+1,
-                        
-                 });
-                }}>button</button>
-      <h2> NAME: {this.props.name}</h2>
-      <h2> LOCATION: {this.props.location}</h2>
-      <h2> CONTACT: sshaikriyaz@gmail.com</h2>
       
-    </div>
-        );
+      //  console.log(props);
+       // console.log(this.props.name+"child constructor");
     }
+  
+
+ async componentDidMount(){
+// the limit of link is over 
+//const data= await fetch("https://api.github.com/users/RIYAZ12697");
+//const json= await data.json();
+ //console.log(json);
+ const json={
+  name:"SHAIK RIYAZ",
+  location:"THORRUR",
+  avatar_url:"https://avatars.githubusercontent.com/u/180375075?v=4"
+ }
+
+ 
+ this.setState({
+   info:json
+ });
+  this.timer =setInterval(()=>{
+  console.log("nmaste react");
+ },1000);
 
 }
+componentDidUpdate(){
+  console.log("called in update phase");
+}
+
+componentWillUnmount(){
+  clearInterval(this.timer)
+
+}
+    render() {
+      //  console.log(this.props.name+"child render");
+      
+        return(
+           
+            <div className="h-60 w-full border border-solid border-black bg-lime-50">
+                
+      <h2> NAME: {this.state.info.name}</h2>
+      <h2> LOCATION: {this.state.info.location}</h2>
+      <h2> CONTACT: sshaikriyaz@gmail.com</h2>
+      <img className="w-36 h-25 border border-solid border-black" src={this.state.info.avatar_url}/>
+    
+    </div>
+    
+        );
+        
+       
+  
+   }
+    }
+
+
 
 export default UserClass;
