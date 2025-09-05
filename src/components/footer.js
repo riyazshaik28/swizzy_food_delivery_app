166aeaefb62e9fcc3445f1e047638937f061e9ba
RIYAZ12697 <sshaikriyaz252@gmail.com>
2025-09-05 20:20:56 +0530

    Implemented footer.js component

---
+++ src/components/footer.js
@@ -0,0 +1,75 @@
+const Footer = ()=>{
+    return(
+         <>    
+    <div className="flex flex-wrap  mt-10 max-w-full bg-orange-50 h-95 text-xs mx-1">
+  
+    <div>
+      <img  className="w-[200px] h-[200px]  mt-5 border border-solid border-black"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsXzCAKQr6cb-qpbMQQHisxWcZ9HE562ivAw&s"></img>
+      <pre>© 2024 kepto Limited</pre>
+    </div>
+    
+    
+        <div className=" mx-3 mt-0 ">
+      <ul className="list-none space-y-2 p-4">
+        <li className="text-xl ">Company</li>
+        <li>About Us</li>
+        <li>Kepto Corporate</li>
+        <li>Careers</li>
+        <li>Team</li>
+        <li>Kepto One</li>
+        <li>Kepto Instamart</li>
+        <li>Kepto Dineout</li>
+        <li>Kepto Genie</li>
+        <li>Minis</li>
+      </ul>
+    </div>
+    
+    <div   className=" mx-3 mt-4 ">
+      <ul className="list-none space-y-2">
+        <li className="text-xl ">Contact us</li>
+        <li>Help & Support</li>
+        <li>Partner with us</li>
+        <li>Ride with us</li>
+      </ul>
+    </div>
+    
+    <div  className=" mx-3   mt-4 ">
+      <ul className="list-none space-y-2">
+        <li  className="text-xl ">Legal</li>
+        <li>Terms & Conditions</li>
+        <li>Cookie Policy</li>
+        <li>Privacy Policy</li>
+      </ul>
+    </div>
+    
+    <div className=" mx-3   mt-4 ">
+      <h3 className="text-xl">Available in:</h3>
+      <ul className="list-none space-y-2">
+        <li>Bangalore</li>
+        <li>Hyderabad</li>
+        <li>Delhi</li>
+        <li>Mumbai</li>
+        <li>Pune</li>
+      </ul>
+      
+      <button className=" h-[20px] w-[80px] border border-solid border-black rounded-e-sm "> 685 cities</button>
+    </div>
+    <div className=" mx-3   mt-4 ">
+      <h4 className="text-xl ">Life at Swiggy</h4>
+      <ul className="list-none space-y-2">
+        <li>Explore with Swiggy</li>
+        <li>Swiggy News</li>
+        <li>Snackables</li>
+      </ul>
+    </div>
+    
+    <diV className="flex border border-solid border-black w-full h-20 bg-red-50 font-medium" >
+       <p className="text-xl   ml-8 mt-5 text-black font-bold">For better experience ,download the kepto app Now</p>
+       <img className="w-[200px] h-10 mt-5 ml-7" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWY6j7XTn2Sf7T-m20sTbYpwa-tt9y5bGntw&s"></img>
+       <img className="w-[200px] h-10 mt-5 ml-7" src="https://cdn1.byjus.com/wp-content/uploads/2018/11/cbse/2016/12/16055302/Download_on_the_App_Store.png"></img>
+    </diV>
+    
+      </div>  </>
+    );
+}
+export default Footer;
