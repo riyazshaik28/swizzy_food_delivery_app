007185cce0fd1724208a9120aaa1a905a66c91cb
RIYAZ12697 <sshaikriyaz252@gmail.com>
2025-09-05 20:17:15 +0530

    Created User.js for user profile management

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,14 @@
 
 const UserCart = (props) => {
   const {name,location}=props;
+  
+
   return (
     <div className="user-cart">
       <h2> NAME: {name}</h2>
       <h2> LOCATION: {location}</h2>
       <h2> CONTACT: sshaikriyaz@gmail.com</h2>
-      <img src={logo} alt={"My Image"} />
+      <img className= "w-36 h-25 border border-solid border-black"src={ "https://avatars.githubusercontent.com/u/180375075?v=4"} alt={"My Image"} />
     </div>
   );
 };
